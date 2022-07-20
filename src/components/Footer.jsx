802e9f78fd0cea9fba670fd79b273a591bd6bfa0
shubhamgohad221997 <101570475+shubhamgohad221997@users.noreply.github.com>
2022-07-21 01:29:17 +0530

    Day-2 Work Add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,101 @@
+import { Box,Image } from '@chakra-ui/react'
+import React from 'react'
+
+import { FaInstagram, FaTwitter, FaYoutube ,FaLinkedinIn } from 'react-icons/fa';
+
+
+
+
+
+const Footer = () => {
+  return (
+    <div>
+        <Box className='foo'>
+      <Box className='foo1'>
+        <ul className='ful'><li><a href="https://www.urbancompany.com/about" target="_blank">About Us</a></li><li><a href="https://www.urbancompany.com/impact" target="_blank">UC Impact</a></li><li><a href="https://www.urbancompany.com/terms" target="_blank">Terms &amp; Conditions</a></li><li><a href="https://www.urbancompany.com/privacy-policy" target="_blank">Privacy Policy</a></li><li><a href="https://www.urbancompany.com/privacy-policy" target="_blank">Interest-Based Advertising</a></li><li><a href="https://www.urbancompany.com/anti-discrimination-policy" target="_blank">Anti Discrimination Policy</a></li><li><a href="https://www.urbancompany.com/blog" target="_blank">Blog</a></li><li><a href="https://www.urbancompany.com/reviews" target="_blank">Reviews</a></li><li><a href="https://www.urbancompany.com/near-me" target="_blank">Near Me</a></li><li><a href="https://careers.urbanclap.com/" target="_blank">Careers</a></li></ul>
+      
+      <ul className='ful'>
+      <li><a href="https://www.urbancompany.com/purchase-giftcards" target="_blank">Gift Cards</a></li><li><a href="https://www.urbancompany.com/contact-us" target="_blank">Contact Us</a></li><li><a href="https://www.urbancompany.com/quick-links" target="_blank">Quick Links</a></li>
+      </ul>
+      <hr className='line'/>
+    
+      </Box>
+    
+        <Box className='foo1'>
+            <p className='p1'>Serving In</p>
+            <p className='ful'>AUS</p>
+            <Box className='foo2'><p className='ful'>Melbourne</p>       <p p className='ful'>Sydney</p></Box>
+        </Box>
+
+            <Box className='foo1'>
+                <p className='p1'>IND</p>
+                <ul className='ful'><li><a href="https://www.urbancompany.com/agra" target="_blank">Agra</a></li><li><a href="https://www.urbancompany.com/ahmedabad" target="_blank">Ahmedabad</a></li><li><a href="https://www.urbancompany.com/alwar" target="_blank">Alwar</a></li><li><a href="https://www.urbancompany.com/amritsar" target="_blank">Amritsar</a></li><li><a href="https://www.urbancompany.com/aurangabad" target="_blank">Aurangabad</a></li><li><a href="https://www.urbancompany.com/bangalore" target="_blank">Bangalore</a></li><li><a href="https://www.urbancompany.com/bhopal" target="_blank">Bhopal</a></li><li><a href="https://www.urbancompany.com/bhubaneswar" target="_blank">Bhubaneswar</a></li><li><a href="https://www.urbancompany.com/chandigarh" target="_blank">Chandigarh Tricity</a></li><li><a href="https://www.urbancompany.com/chennai" target="_blank">Chennai</a></li><li><a href="https://www.urbancompany.com/coimbatore" target="_blank">Coimbatore</a></li></ul>
+                <ul className='ful'> <li><a href="https://www.urbancompany.com/cuttack" target="_blank">Cuttack</a></li><li><a href="https://www.urbancompany.com/dehradun" target="_blank">Dehradun</a></li><li><a href="https://www.urbancompany.com/delhi-ncr" target="_blank">Delhi NCR</a></li><li><a href="https://www.urbancompany.com/guntur" target="_blank">Guntur</a></li><li><a href="https://www.urbancompany.com/guwahati" target="_blank">Guwahati</a></li><li><a href="https://www.urbancompany.com/gwalior" target="_blank">Gwalior</a></li><li><a href="https://www.urbancompany.com/hyderabad" target="_blank">Hyderabad</a></li>
+                <li><a href="https://www.urbancompany.com/indore" target="_blank">Indore</a></li><li><a href="https://www.urbancompany.com/jabalpur" target="_blank">Jabalpur</a></li><li><a href="https://www.urbancompany.com/jaipur" target="_blank">Jaipur</a></li><li><a href="https://www.urbancompany.com/jamshedpur" target="_blank">Jamshedpur</a></li><li><a href="https://www.urbancompany.com/jodhpur" target="_blank">Jodhpur</a></li></ul>
+                
+                <ul className='ful'> <li><a href="https://www.urbancompany.com/kakinada" target="_blank">Kakinada</a></li><li><a href="https://www.urbancompany.com/kanpur" target="_blank">Kanpur</a></li><li><a href="https://www.urbancompany.com/karnal" target="_blank">Karnal</a></li><li><a href="https://www.urbancompany.com/kochi" target="_blank">Kochi</a></li><li><a href="https://www.urbancompany.com/kolkata" target="_blank">Kolkata</a></li><li><a href="https://www.urbancompany.com/kota" target="_blank">Kota</a></li><li><a href="https://www.urbancompany.com/lucknow" target="_blank">Lucknow</a></li><li><a href="https://www.urbancompany.com/ludhiana" target="_blank">Ludhiana</a></li><li><a href="https://www.urbancompany.com/madurai" target="_blank">Madurai</a></li><li><a href="https://www.urbancompany.com/meerut" target="_blank">Meerut</a></li><li><a href="https://www.urbancompany.com/mumbai" target="_blank">Mumbai</a></li><li><a href="https://www.urbancompany.com/mysore" target="_blank">Mysore</a></li><li><a href="https://www.urbancompany.com/nagpur" target="_blank">Nagpur</a></li></ul>
+
+                    <ul className='ful'> <li><a href="https://www.urbancompany.com/nashik" target="_blank">Nashik</a></li><li><a href="https://www.urbancompany.com/panipat" target="_blank">Panipat</a></li><li><a href="https://www.urbancompany.com/patna" target="_blank">Patna</a></li><li><a href="https://www.urbancompany.com/prayagraj" target="_blank">Prayagraj</a></li><li><a href="https://www.urbancompany.com/pune" target="_blank">Pune</a></li><li><a href="https://www.urbancompany.com/raipur" target="_blank">Raipur</a></li><li><a href="https://www.urbancompany.com/rajahmundry" target="_blank">Rajahmundry</a></li><li><a href="https://www.urbancompany.com/ranchi" target="_blank">Ranchi</a></li><li><a href="https://www.urbancompany.com/rohtak" target="_blank">Rohtak</a></li><li><a href="https://www.urbancompany.com/sonipat" target="_blank">Sonipat</a></li><li><a href="https://www.urbancompany.com/surat" target="_blank">Surat</a></li><li><a href="https://www.urbancompany.com/thiruvananthapuram" target="_blank">Thiruvananthapuram</a></li></ul>
+
+                <ul className='ful'> <li><a href="https://www.urbancompany.com/udaipur" target="_blank">Udaipur</a></li><li><a href="https://www.urbancompany.com/vadodara" target="_blank">Vadodara</a></li><li><a href="https://www.urbancompany.com/varanasi" target="_blank">Varanasi</a></li><li><a href="https://www.urbancompany.com/vijayawada" target="_blank">Vijayawada</a></li><li><a href="https://www.urbancompany.com/visakhapatnam" target="_blank">Visakhapatnam</a></li><li><a href="https://www.urbancompany.com/warangal" target="_blank">Warangal</a></li></ul>
+             
+            </Box>
+
+
+            <Box className='foo1'>
+            
+            <p className='p1'>KSA</p>
+            <Box className='foo2'><p className='ful'>Jeddah</p>       <p p className='ful'>Riyadh</p></Box>
+            </Box>
+
+
+            <Box className='foo1'>
+            
+            <p className='p1'>UAE</p>
+            <Box className='foo2'><p className='ful'>Abu Dhabi</p>   <p className='ful'>Dubai</p>     <p p className='ful'>Sharjah</p></Box>
+            </Box>
+
+            <Box className='foo1'>
+            
+            <p className='p1'>USA</p>
+            <Box className='foo2'><p className='ful'>Austin</p>  </Box>
+            <hr className='line'/>
+            </Box>
+
+          
+            <Box className='foo12'>
+                        <Box>
+                        <Image className="fimg" src="https://res.cloudinary.com/urbanclap/image/upload/images/growth/home-screen/1631097450980-d2de38.png"></Image>
+                        </Box>
+                        <Box><p className='p2'>© 2014-22 UrbanClap Technologies India Pvt. Ltd.</p></Box>
+                        <Box className='icon'>
+
+
+<a href="/"><FaTwitter className='io' /></a>
+<a href="/"><FaYoutube className='io' />  </a>
+<a href="/"><FaInstagram className='io' /></a>
+<a href="/"><FaLinkedinIn className='io'/></a>
+
+
+
+
+
+                            
+                        </Box>
+                        <Box className='foo22'>
+
+                            <Image src="https://images.urbanclap.com/image/upload/categories/category_v2/category_7f907eb0.png"></Image>
+                        <Image src="https://images.urbanclap.com/image/upload/categories/category_v2/category_7f741d10.png"></Image>
+                        </Box>
+
+
+            </Box>
+
+
+      </Box>
+    </div>
+  )
+}
+
+export default Footer
+
